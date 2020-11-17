d0bb54cf5d1c66d1f1687c7797837b0a72db0abb
Tara Mathers <matherst@ca.ibm.com>
2020-11-17 19:24:06 -0400

    Add score text, camera resize script, tiled background, new assets

--- Assets/BallMotion.cs
+++ Assets/BallMotion.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BallMotion : MonoBehaviour {
 
@@ -8,17 +9,27 @@ public class BallMotion : MonoBehaviour {
     private Rigidbody2D rb2d;
     private AudioSource hitSound;
     private Collider2D ballCollider;
+    private TextMeshProUGUI scoreText;
 
     public float speed;
     public float maxSpeed;
+    private int score = 0;
 
     // If the velocity is between +/- this, then change the direction randomly
     private float zeroVelocitySensitivity = 1.0f;
- 
+
+
     void Start () {
         rb2d = GetComponent<Rigidbody2D>();
         hitSound = GetComponent<AudioSource>();
         ballCollider = GetComponent<Collider2D>();
+        GameObject obj = GameObject.Find("/SampleScene");
+        
+        // Score text
+        obj = GameObject.Find("ScoreText");
+        //Debug.Log("OBJ: " + obj.name);
+        scoreText = obj.GetComponent<TextMeshProUGUI>();
+        scoreText.text = "0";
 
         // set the ball's initial speed
         Vector2 direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
@@ -27,6 +38,7 @@ public class BallMotion : MonoBehaviour {
         rb2d.velocity = direction * speed;
     }
  
+
     // Update is called once per frame
     void Update() {
         
@@ -47,10 +59,25 @@ public class BallMotion : MonoBehaviour {
     // When the ball is hit
     private void hit(Vector2 point) {
 
+        Debug.Log("clicked: " + point.x + ", " + point.y);
+        Debug.Log("Ball:    " + rb2d.position.x + ", " + rb2d.position.y);
+
+
+        float distance = Mathf.Sqrt(
+            Mathf.Pow(rb2d.position.x - point.x, 2) * 
+            Mathf.Pow(rb2d.position.y - point.y, 2));
+        Debug.Log("Distance: " + distance);
+
         // Check if there is a collision
-        if (collider2D.OverlapPoint(point)) {
+        if (distance < 0.5) { //ballCollider.OverlapPoint(point)) { didn't work for some reason
             Debug.Log("Collision Detected !");
+            
+            // Update score
+            score += 10;
+            scoreText.text = "" + score;
+            
             hitSound.Play();
+
         }
     }
 
@@ -69,7 +96,7 @@ public class BallMotion : MonoBehaviour {
             rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxSpeed);
         }
 
-        Debug.Log("X = " + rb2d.velocity.x + ", Y = " + rb2d.velocity.y);
+        //Debug.Log("X = " + rb2d.velocity.x + ", Y = " + rb2d.velocity.y);
 
 
         if (rb2d.velocity.x < zeroVelocitySensitivity && rb2d.velocity.x > -zeroVelocitySensitivity) {
---
+++ Assets/SetupCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupCamera : MonoBehaviour {
+
+    public float targetAspect;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        // Set the camera scale
+
+        float windowAspect = (float)Screen.width / (float)Screen.height;
+        float scaleHeight = windowAspect / targetAspect;
+        Camera camera = GetComponent<Camera>();
+
+        if (scaleHeight < 1.0f) {  
+            camera.orthographicSize = camera.orthographicSize / scaleHeight;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
